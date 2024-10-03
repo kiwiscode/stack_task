a36dbba02d69e2e68a7b9668594f48e47e512120
kiwiscode <ayktkav@gmail.com>
2024-10-03 18:20:56 +0200

    seperating deployment and local version with url

--- server/config/index.js
+++ server/config/index.js
@@ -19,9 +19,6 @@ module.exports = (app) => {
   // CORS MIDDLEWARE INSIDE module.exports TO ALLOW CROSS-ORIGIN INTERACTION:
   app.use(
     cors({
-      // when working on local version
-      // origin: "http://localhost:5173",
-      // when working on deployment version
       origin: process.env.FRONTEND_URL,
     })
   );
